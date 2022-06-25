88c6f8e61bfc6d859ce3797c259aef2b1061f60c
Alejandro Vivas <alejandrojv17@gmail.com>
2022-06-25 15:34:59 -0400

    Dark mode toggler button

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -107,11 +107,16 @@ export default function Header() {
             </Link>
             <Link href="/">
               <a
-                className="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700"
+                className="inline-flex items-center justify-center px-4 py-2 ml-4 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-cyan-500 whitespace-nowrap hover:bg-cyan-600"
               >
                 Contact
               </a>
             </Link>
+            <button className="inline-flex items-center justify-center px-4 py-2 ml-4 text-base font-medium text-gray-500 border border-transparent rounded-md shadow-sm hover:text-gray-700 whitespace-nowrap ">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
